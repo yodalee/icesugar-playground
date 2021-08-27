accf81cf997f99eea717d47f53dd2cd37c686b49
yodalee <lc85301@gmail.com>
2021-08-27 23:13:37 +0800

    [UART] add uart.v, keep send 0

--- uart/top.v
+++ uart/top.v
@@ -1,5 +1,6 @@
 module top (
   input clk,
+  input rst,
   output reg uart_tx,
 );
 
@@ -11,13 +12,36 @@ reg clk_9600 = 0;
 reg [16:0] cnt_9600 = 32'b0;
 localparam CNT_RESET_9600 = (clk_freq / baudrate / 2);
 
+reg [7:0] bytes = "0";
+reg uart_send = 1'b1; // always start transmit
+wire uart_txed;
+
+uart mod_uart (
+  /* input */
+  .clk_baud(clk_9600),
+  .rst(rst),
+  .tx_byte(bytes),
+  .tx_send(uart_send),
+
+  /* output */
+  .tx(uart_tx),
+  .tx_done(uart_txed),
+);
+
 always @(posedge clk) begin
+  if (!rst) begin
+    bytes <= "0";
+  end
+  else begin
+
   if (cnt_9600 == 0) begin
     clk_9600 <= ~clk_9600;
     cnt_9600 <= CNT_RESET_9600;
   end
   else begin
     cnt_9600 <= cnt_9600 - 1;
+  end
+
   end
 end
 
