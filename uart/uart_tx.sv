fa500360857d980554248fdff48f05b5df0b1a55
yodalee <lc85301@gmail.com>
2021-09-02 00:37:59 +0800

    [UART] separate next state logic of each variable

--- uart/uart_tx.sv
+++ uart/uart_tx.sv
@@ -21,6 +21,7 @@ typedef enum logic [1:0] {
 State_t state = STATE_IDLE, state_next;
 byte tx_buf, tx_buf_next;
 byte tx_cnt, tx_cnt_next;
+int  clk_cnt, clk_cnt_next;
 
 /* output logic */
 assign done = state == STATE_DONE;
@@ -38,57 +39,62 @@ always_comb begin
   endcase
 end
 
-/* next state */
+/* next logic for state */
 always_comb begin
   case (state)
     STATE_IDLE: begin
-      if (start_send == 1'b1) begin
-        state_next = STATE_START;
-        tx_buf_next = tx_byte;
-        tx_cnt_next = 8'b0;
-      end
-      else begin
-        state_next = STATE_IDLE;
-        tx_buf_next = 8'b0;
-        tx_cnt_next = 8'b0;
-      end
+      if (start_send == 1'b1) begin state_next = STATE_START; end
+      else begin state_next = state; end
     end
     STATE_START: begin
       state_next = STATE_TXING;
-      tx_buf_next = tx_buf;
-      tx_cnt_next = 8'b0;
     end
     STATE_TXING: begin
-      if (tx_cnt < 8'd8) begin
-        state_next = STATE_TXING;
-        tx_buf_next = tx_buf >> 1;
-        tx_cnt_next = tx_cnt+1;
-      end
-      else begin
-        state_next = STATE_DONE;
-        tx_buf_next = 8'b0;
-        tx_cnt_next = 8'b0;
-      end
+      if (tx_cnt < 8'd8) begin state_next = state; end
+      else begin state_next = STATE_DONE; end
     end
     default: begin
       state_next = STATE_IDLE;
-      tx_buf_next = 8'b0;
-      tx_cnt_next = 8'b0;
     end
   endcase
+end
+
+/* next logic for tx_buf */
+always_comb begin
+  if (state == STATE_IDLE && start_send == 1'b1) begin
+    tx_buf_next = tx_byte;
+  end
+  else if (state == STATE_TXING && tx_cnt < 8) begin
+    tx_buf_next = tx_buf >> 1;
+  end
+  else begin
+    tx_buf_next = tx_buf;
+  end
+end
+
+/* next logic for tx_cnt */
+always_comb begin
+  if (state == STATE_TXING && tx_cnt < 8) begin
+    tx_cnt_next = tx_cnt+1;
+  end
+  else begin
+    tx_cnt_next = 0;
+  end
 end
 
 /* update state logic */
 always_ff @(posedge clk_baud or negedge rst) begin
   if (!rst) begin
-    state <= STATE_IDLE;
-    tx_buf <= 8'b0;
-    tx_cnt <= 8'b0;
+    state   <= STATE_IDLE;
+    tx_buf  <= 0;
+    tx_cnt  <= 0;
+    clk_cnt <= 0;
   end
   else begin
-    state <= state_next;
-    tx_buf <= tx_buf_next;
-    tx_cnt <= tx_cnt_next;
+    state   <= state_next;
+    tx_buf  <= tx_buf_next;
+    tx_cnt  <= tx_cnt_next;
+    clk_cnt <= clk_cnt_next;
   end
 end
 
