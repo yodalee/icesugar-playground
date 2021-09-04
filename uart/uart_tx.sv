70a5f58063ad839183fb6b8510b56f8906c9c566
yodalee <lc85301@gmail.com>
2021-09-04 22:50:19 +0800

    [UART] refactor, make START/DONE part of TX

--- uart/uart_tx.sv
+++ uart/uart_tx.sv
@@ -12,33 +12,20 @@ module uart (
 
 parameter CLK_PER_BAUD = 1;
 localparam CLK_CNT_LIMIT = CLK_PER_BAUD-1;
+localparam TX_LENGTH = 10; // start 1'b0, 1 byte, end 1'b1
 
-typedef enum logic [1:0] {
+typedef enum logic {
   STATE_IDLE  = 0,
-  STATE_START = 1,
-  STATE_TXING = 2,
-  STATE_DONE  = 3
+  STATE_TXING = 1
 } State_t;
 State_t state = STATE_IDLE, state_next;
-byte tx_buf, tx_buf_next;
-byte tx_cnt, tx_cnt_next;
+logic [9:0] tx_buf;
+logic [3:0] tx_idx, tx_idx_next;
 int  clk_cnt, clk_cnt_next;
 
 /* output logic */
-assign done = state == STATE_DONE;
-always_comb begin
-  case (state)
-    STATE_START: begin
-      tx = 1'b0;
-    end
-    STATE_TXING: begin
-      tx = tx_buf[0];
-    end
-    default: begin
-      tx = 1'b1;
-    end
-  endcase
-end
+assign done = tx_idx == TX_LENGTH - 1;
+assign tx = (state == STATE_IDLE) ? 1'b1 : tx_buf[tx_idx];
 
 /* next logic for clk_cnt */
 always_comb begin
@@ -49,48 +36,27 @@ end
 always_comb begin
   case (state)
     STATE_IDLE: begin
-      state_next = (start_send == 1'b1) ? STATE_START : state;
-    end
-    STATE_START: begin
-      if (clk_cnt < CLK_CNT_LIMIT) begin state_next = state; end
-      else begin state_next = STATE_TXING; end
+      state_next = (start_send == 1'b1) ? STATE_TXING : state;
     end
     STATE_TXING: begin
-      if (clk_cnt == CLK_CNT_LIMIT && tx_cnt == 7) begin state_next = STATE_DONE; end
+      if (clk_cnt == CLK_CNT_LIMIT && tx_idx == TX_LENGTH-1) begin state_next = STATE_IDLE; end
       else begin state_next = state; end
-    end
-    STATE_DONE: begin
-      if (clk_cnt < CLK_CNT_LIMIT) begin state_next = state; end
-      else begin state_next = STATE_IDLE; end
     end
   endcase
 end
 
-/* next logic for tx_buf */
+/* next logic for tx_idx */
 always_comb begin
-  if (state == STATE_IDLE && start_send == 1'b1) begin
-    tx_buf_next = tx_byte;
-  end
-  else if (state == STATE_TXING && tx_cnt < 8 && clk_cnt == CLK_CNT_LIMIT) begin
-    tx_buf_next = tx_buf >> 1;
-  end
-  else begin
-    tx_buf_next = tx_buf;
-  end
-end
-
-/* next logic for tx_cnt */
-always_comb begin
-  if (state == STATE_TXING && tx_cnt < 8) begin
+  if (state == STATE_TXING && tx_idx < TX_LENGTH) begin
     if (clk_cnt == CLK_CNT_LIMIT) begin
-      tx_cnt_next = tx_cnt+1;
+      tx_idx_next = tx_idx+1;
     end
     else begin
-      tx_cnt_next = tx_cnt;
+      tx_idx_next = tx_idx;
     end
   end
   else begin
-    tx_cnt_next = 0;
+    tx_idx_next = 0;
   end
 end
 
@@ -99,13 +65,15 @@ always_ff @(posedge clk or negedge rst) begin
   if (!rst) begin
     state   <= STATE_IDLE;
     tx_buf  <= 0;
-    tx_cnt  <= 0;
+    tx_idx  <= 0;
     clk_cnt <= 0;
   end
   else begin
+    if (state == STATE_IDLE && start_send) begin
+      tx_buf <= {1'b1, tx_byte, 1'b0};
+    end
     state   <= state_next;
-    tx_buf  <= tx_buf_next;
-    tx_cnt  <= tx_cnt_next;
+    tx_idx  <= tx_idx_next;
     clk_cnt <= clk_cnt_next;
   end
 end
